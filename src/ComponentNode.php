c05375aa07063f7789d144e142d8fc935b248b5a
Giorgio Pogliani <giorgio.pogliani@performingdigital.com>
2021-03-04 16:53:15 +0100

    Fix twig compatibilty

--- src/ComponentNode.php
+++ src/ComponentNode.php
@@ -57,7 +57,7 @@ final class ComponentNode extends IncludeNode
         ;
     }
 
-    public function getTemplateName()
+    public function getTemplateName(): ?string
     {
         return $this->getAttribute('path');
     }

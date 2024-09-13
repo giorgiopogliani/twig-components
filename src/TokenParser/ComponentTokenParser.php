be2dbeee1c5a3519fb8c3978df2923014e0aa152
Marco <marco.messa3@gmail.com>
2024-09-13 12:14:11 +0200

    Update ComponentTokenParser.php
    Is enabled hint path support, prepare the hint path

--- src/TokenParser/ComponentTokenParser.php
+++ src/TokenParser/ComponentTokenParser.php
@@ -31,8 +31,13 @@ final class ComponentTokenParser extends IncludeTokenParser
 
         $componentPath = rtrim($this->configuration->getTemplatesPath(), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . $name;
 
-        if ($this->configuration->isUsingTemplatesExtension()) {
-            $componentPath .= '.' . $this->configuration->getTemplatesExtension();
+        if ($this->configuration->getNeedsHintPath()) {
+            $componentPath = rtrim($componentPath, '.');
+            $componentPath = str_replace('/', '.', $componentPath);
+        } else {
+            if ($this->configuration->isUsingTemplatesExtension()) {
+                $componentPath .= '.' . $this->configuration->getTemplatesExtension();
+            }
         }
 
         return  $componentPath;
